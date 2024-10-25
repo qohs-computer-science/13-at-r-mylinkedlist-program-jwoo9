aca18714bfd6d9405208486e65ee4a9a3f945860
jwoo9 <jwyutong@gmail.com>
2024-10-25 14:00:15 +0000

    working - 10/25/24

--- MyLLTester.java
+++ MyLLTester.java
@@ -1,8 +1,26 @@
-public class MyLLTester.java
+public class MyLLTester
 {
   public static void main(String [] args)
   {
-    
+    MyLinkedList myLL = new MyLinkedList();
+    System.out.println(myLL.size());
+    System.out.println(myLL.isEmpty());
+    myLL.add("test");
+    myLL.add("test2");
+    myLL.addLast("testLast");
+    myLL.addFirst("testFirst");
+    System.out.println(myLL.toString());
+    System.out.println(myLL.size());
+    System.out.println(myLL.isEmpty());
+    System.out.println(myLL.get(0));
+    System.out.println(myLL.get(2));
+    System.out.println(myLL.get(3));
+    System.out.println(myLL.get(1));
+    myLL.set(0,"newFirst");
+    myLL.set(3,"newLast");
+    myLL.set(2,"test3");
+    System.out.println(myLL);
+
 
   } // end main
   
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,82 @@
 public class MyLinkedList
 {
+    private ListNode head;
+    private int size;
 
+    public MyLinkedList()
+    {
+        head = null;
+        size = 0;
+    } //end constructor
 
-}
+    public int size()
+    {
+        return size;
+    } //end method size
+
+    public boolean isEmpty()
+    {
+        if (size == 0)
+            return true;
+        return false;
+    } //end method isEmpty
+
+    public boolean add(Object newItem)
+    {
+        ListNode temp=head;
+        if (size == 0)
+            head = new ListNode(newItem,null);
+        else{
+            while (temp.getNext()!=null)
+                temp = temp.getNext();
+            temp.setNext(new ListNode(newItem, null));
+        }//end else
+        size++;
+        return true;
+    }//end method add
+
+    public boolean addFirst(Object newItem){
+        head = new ListNode(newItem, head);
+        size++;
+        return true;
+    }//end method addFirst
+
+    public boolean addLast(Object newItem){
+        add(newItem);
+        return true;
+    } //end method addLast
+
+    public Object get(int i){
+        ListNode temp = head;
+        if (i<0 || i>=size)
+            throw new IndexOutOfBoundsException();
+        for (int x = 0; x<i;x++)
+            temp = temp.getNext();
+        return temp.getValue();
+    } //end method get
+
+    public Object set(int i, Object newValue){
+        ListNode temp = head;
+        if (i<0 || i>=size)
+            throw new IndexOutOfBoundsException();
+        for (int x = 0; x<i;x++)
+            temp = temp.getNext();
+        Object retVal = temp.getValue();
+        temp.setValue(newValue);
+        return retVal;
+    } //end method set
+
+    public String toString()
+    {
+        String retVal = "";
+        int position = 1;
+        ListNode temp = head;
+        while (temp != null){
+            retVal += position + ": " + temp.getValue() + "\n";
+            temp=temp.getNext();
+            position++;
+        } //end while loop\
+        return retVal;
+    } //end toString
+
+} //end class MyLinkedList
